e105de387705ac4f65d0069e08ccae2f3e7ebc93
YaroslavSahach <49754839+YaroslavSahach@users.noreply.github.com>
2020-02-24 17:45:07 +0200

    added warning to verification plugin (#49)
    * added warning to verification plugin
    
    * added relax time

--- sbt/sbt-izumi/src/main/scala/izumi/sbt/plugins/SbtgenVerificationPlugin.scala
+++ sbt/sbt-izumi/src/main/scala/izumi/sbt/plugins/SbtgenVerificationPlugin.scala
@@ -1,8 +1,13 @@
 package izumi.sbt.plugins
 
+import sbt.internal.util.ConsoleLogger
 import sbt.{Def, _}
 
+import scala.concurrent.duration._
+
 object SbtgenVerificationPlugin extends AutoPlugin {
+  protected val logger: ConsoleLogger = ConsoleLogger()
+
   override def globalSettings: Seq[Def.Setting[_]] = {
 
     val scripts = file("project") ** "*.sc"
@@ -13,10 +18,15 @@ object SbtgenVerificationPlugin extends AutoPlugin {
       .map(f => (f, f.lastModified()))
 
     val buildsbt = new File("build.sbt").lastModified()
-    val bad = sbtgen.filter(_._2 > buildsbt)
+    val relaxTime = Option(System.getProperty("sbtgen.modificationErrorRelaxTime")).map(_.toInt).getOrElse(30)
+    val bad = sbtgen.filter(_._2 - buildsbt > relaxTime.seconds.toMillis)
 
     if (bad.nonEmpty) {
-      throw new RuntimeException(s"sbtgen definitions are newer than build.sbt, run `sbtgen.sc`: ${bad.map(_._1)}")
+      val message = s"sbtgen definitions are newer than build.sbt, run `sbtgen.sc`: ${bad.map(_._1)}"
+      if (Option(System.getProperty("sbtgen.modificationError")).forall(_.toBoolean))
+        throw new RuntimeException(message)
+      else
+        logger.warn(message)
     }
 
     Seq.empty
