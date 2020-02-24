2da829832cf3fd8c677392249811de8aa541ab7e
YaroslavSahach <ysahach@playq.net>
2020-02-24 17:16:15 +0200

    added relax time

--- sbt/sbt-izumi/src/main/scala/izumi/sbt/plugins/SbtgenVerificationPlugin.scala
+++ sbt/sbt-izumi/src/main/scala/izumi/sbt/plugins/SbtgenVerificationPlugin.scala
@@ -3,6 +3,8 @@ package izumi.sbt.plugins
 import sbt.internal.util.ConsoleLogger
 import sbt.{Def, _}
 
+import scala.concurrent.duration._
+
 object SbtgenVerificationPlugin extends AutoPlugin {
   protected val logger: ConsoleLogger = ConsoleLogger()
 
@@ -16,7 +18,8 @@ object SbtgenVerificationPlugin extends AutoPlugin {
       .map(f => (f, f.lastModified()))
 
     val buildsbt = new File("build.sbt").lastModified()
-    val bad = sbtgen.filter(_._2 > buildsbt)
+    val relaxTime = Option(System.getProperty("sbtgen.modificationErrorRelaxTime")).map(_.toInt).getOrElse(30)
+    val bad = sbtgen.filter(_._2 - buildsbt > relaxTime.seconds.toMillis)
 
     if (bad.nonEmpty) {
       val message = s"sbtgen definitions are newer than build.sbt, run `sbtgen.sc`: ${bad.map(_._1)}"
