e39441ff5efcf2cbac19f165f9d73737953a41f4
Kai <450507+neko-kai@users.noreply.github.com>
2020-05-17 19:16:45 +0100

    default to Scala.js 1.0.0 (#50)
    * default to Scala.js 1.0.0
    
    * Update plugins.sbt

--- sbtgen/src/main/scala/izumi/sbtgen/model/GlobalSettings.scala
+++ sbtgen/src/main/scala/izumi/sbtgen/model/GlobalSettings.scala
@@ -3,9 +3,9 @@ package izumi.sbtgen.model
 case class GlobalSettings(
                            groupId: String,
                            sbtVersion: Option[String] = Some("1.3.10"),
-                           scalaJsVersion: Version = Version.VConst("0.6.32"),
+                           scalaJsVersion: Version = Version.VConst("1.1.0"),
                            scalaNativeVersion: Version = Version.VConst("0.4.0-M2"),
                            crossProjectVersion: Version = Version.VConst("1.0.0"),
-                           bundlerVersion: Option[Version] = Some(Version.VConst("0.14.0")),
+                           bundlerVersion: Option[Version] = Some(Version.VConst("0.17.0")),
                            sbtDottyVersion: Version = Version.VConst("0.4.1"),
                          )
--- test/js/project/plugins.sbt
+++ test/js/project/plugins.sbt
@@ -1,11 +1,11 @@
 // https://www.scala-js.org/
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.32")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.1.0")
 
 // https://github.com/portable-scala/sbt-crossproject
 addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.0.0")
 
 // https://scalacenter.github.io/scalajs-bundler/
-addSbtPlugin("ch.epfl.scala" % "sbt-scalajs-bundler" % "0.14.0")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalajs-bundler" % "0.17.0")
 
 ////////////////////////////////////////////////////////////////////////////////
 
