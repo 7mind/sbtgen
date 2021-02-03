c11ed2e554c2066269b447a36bb50954d2c3d028
Kai <kai@7mind.io>
2021-02-03 16:40:31 +0000

    Make non-exhaustive matches fatal with default scalac options

--- sbtgen/src/main/scala/izumi/sbtgen/Defaults.scala
+++ sbtgen/src/main/scala/izumi/sbtgen/Defaults.scala
@@ -72,6 +72,7 @@ object Defaults {
     "-Ypartial-unification", // 2.12 only
     CRaw("""if (insideCI.value) "-Wconf:any:error" else "-Wconf:any:warning""""), // enable fatal warnings on CI
     "-Wconf:cat=optimizer:warning", // make optimizer (inliner) warnings non-fatal
+    "-Wconf:cat=other-match-analysis:error", // make non-exhaustive matches fatal
     "-Wconf:msg=kind-projector:silent", // ignore kind-projector's deprecation of `?` syntax
 
     "-Ybackend-parallelism", CRaw("math.min(16, math.max(1, sys.runtime.availableProcessors() - 1)).toString"),
@@ -123,6 +124,7 @@ object Defaults {
     "-Xlint:_,-eta-sam,-multiarg-infix,-byname-implicit", // byname-implicit false positives: https://github.com/scala/bug/issues/12072
     CRaw("""if (insideCI.value) "-Wconf:any:error" else "-Wconf:any:warning""""), // enable fatal warnings on CI
     "-Wconf:cat=optimizer:warning", // make optimizer (inliner) warnings non-fatal
+    "-Wconf:cat=other-match-analysis:error", // make non-exhaustive matches fatal
     "-Wconf:msg=kind-projector:silent", // ignore kind-projector's deprecation of `?` syntax
 
     "-Ybackend-parallelism", CRaw("math.min(16, math.max(1, sys.runtime.availableProcessors() - 1)).toString"),
