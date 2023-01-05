d3e8b8ec35e0ada9326e5c5a42d8e2ba847a64db
Pavel Shirshov <pshirshov@eml.cc>
2023-01-05 17:50:54 +0000

    gha: wip

--- .build.sh
+++ .build.sh
@@ -40,13 +40,6 @@ function publish {
 }
 
 function init {
-    echo "=== INIT ==="
-    if [[ "$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER" == ""  ]] ; then
-        export CI_PULL_REQUEST=false
-    else
-        export CI_PULL_REQUEST=true
-    fi
-
     export USERNAME=${USER:-`whoami`}
     export COURSIER_CACHE=${COURSIER_CACHE:-`~/.coursier`}
     export IVY_CACHE_FOLDER=${IVY_CACHE_FOLDER:-`~/.ivy2`}
@@ -54,19 +47,11 @@ function init {
     export SCALA212=$(cat projects/ScalaVersions.scala | grep 'scala_212' |  sed -r 's/.*\"(.*)\".**/\1/')
     export SCALA213=$(cat projects/ScalaVersions.scala | grep 'scala_213' |  sed -r 's/.*\"(.*)\".**/\1/')
 
-    printenv
-
     git config --global user.name "$USERNAME"
     git config --global user.email "$CI_BUILD_UNIQ_SUFFIX@$CI_COMMIT"
     git config --global core.sshCommand "ssh -t -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-    echo "pwd: `pwd`"
-    echo "Current directory:"
-    ls -la .
-    echo "Home:"
-    ls -la ~
-
-    echo "=== END ==="
+    printenv
 }
 
 function secrets {
