5cdbec9f387b52ff96b7c154084007bc6165ab07
Scala Steward <scala_steward@virtuslab.com>
2024-03-02 13:44:36 +0000

    Update scala3-library to 3.3.3

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -1,7 +1,7 @@
 object ScalaVersions {
   val scala_212 = "2.12.19"
   val scala_213 = "2.13.13"
-  val scala_3 = "3.2.2"
+  val scala_3 = "3.3.3"
 
   val scalaJsVersion = "1.15.0"
   val scalaNativeVersion = "0.4.17"
