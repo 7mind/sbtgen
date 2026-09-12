b24e57035bc888bf16fd8a4488ae21c8985d1c97
Scala Steward <scala_steward@virtuslab.com>
2026-09-12 13:05:56 +0000

    Update sbt-scalajs to 1.22.0

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -3,6 +3,6 @@ object ScalaVersions {
   val scala_213 = "2.13.18"
   val scala_3 = "3.3.7"
 
-  val scalaJsVersion = "1.21.0"
+  val scalaJsVersion = "1.22.0"
   val scalaNativeVersion = "0.5.10"
 }
